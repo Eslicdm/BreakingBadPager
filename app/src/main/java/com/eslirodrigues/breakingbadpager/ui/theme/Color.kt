ce4787f6503c3e00d9fe2ce68b98b8f43a415a0c
Eslicdm <eslicdm1@gmail.com>
2021-12-10 17:39:54 -0300

    first commit

---
+++ app/src/main/java/com/eslirodrigues/breakingbadpager/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.eslirodrigues.breakingbadpager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val DarkYellow = Color(0xFFc6a700)
+val PrimaryYellow = Color(0xFFfdd835)
+val LightYellow = Color(0xFFffff6b)
